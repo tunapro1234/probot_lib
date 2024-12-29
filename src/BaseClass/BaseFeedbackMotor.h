31c520be4f4cee3e6d40380871f221c64ba60f17
Tuna Gül <tuna.gul@nfrproducts.com>
2024-12-29 08:26:14 +0300

    Update base feedback motor

--- src/BaseClass/BaseFeedbackMotor.h
+++ src/BaseClass/BaseFeedbackMotor.h
@@ -1,67 +1,36 @@
 #ifndef BaseFeedbackMotor_h
 #define BaseFeedbackMotor_h
 
-#include "BaseMotorController.h"
-#include "BaseEncoder.h"
-#include "../Controllers/PIDController.h"
-#include "../UpdateHelper/UpdateHelper.h"
-
-// Enumeration for drive modes
-enum class DriveMode {
-    PowerDrive,
-    SpeedDrive,
-    PositionalDrive
-};
+#include "../Controller/PIDCoefficients.h"
 
 class BaseFeedbackMotor {
 public:
-    // Constructor with dependencies
-    BaseFeedbackMotor(BaseMotorController& motorController, BaseEncoder& encoder, PIDController& pidController,
-                     unsigned long updateInterval = 100, unsigned long maxDelay = 200)
-        : motor(motorController), encoder(encoder), pid(pidController),
-          currentMode(DriveMode::PowerDrive), targetValue(0.0f),
-          updateHelper(updateInterval, maxDelay) {}
-
     virtual ~BaseFeedbackMotor() {}
 
-    // Initialize the motor and encoder
     virtual void begin() = 0;
-
-    // Set target based on mode
-    virtual void setTarget(float target) = 0;
-
-    // Update motor control based on current mode
     virtual void update() = 0;
+    virtual void reset() = 0;
 
-    // Set the current drive mode
-    void setDriveMode(DriveMode mode) {
-        currentMode = mode;
-        pid.reset();
-    }
+    virtual void setPositionTarget(float target) = 0;
+    virtual void setVelocityTarget(float target) = 0;
 
-    // Get the current position from the encoder
-    long getPosition() {
-        return encoder.getCount();
-    }
+    virtual bool isAtTarget() = 0;
+    virtual void setPositionTolerance(float tolerance) = 0;
+    virtual void setVelocityTolerance(float tolerance) = 0;
 
-    // Set PID parameters
-    void setPID(float kp, float ki, float kd) {
-        pid.setTunings(kp, ki, kd);
-    }
+    virtual double getVelocity() = 0;
+    virtual double getPosition() = 0;
 
-protected:
-    BaseMotorController& motor;
-    BaseEncoder& encoder;
-    PIDController& pid;
-    DriveMode currentMode;
-    float targetValue;
-    UpdateHelper updateHelper;
+    virtual double getVelocityTarget() = 0;
+    virtual double getPositionTarget() = 0;
 
-    // Compute PID output
-    float computePID(float currentValue) {
-        pid.setSetpoint(targetValue);
-        return pid.compute(currentValue);
-    }
+    // Set PID Coefficients
+    virtual void setPositionPID(const PIDCoefficients& coeffs) = 0;
+    virtual void setVelocityPID(const PIDCoefficients& coeffs) = 0;
+
+    // Get Active Mode
+    virtual bool isPositional() const = 0;
+    virtual bool isVelocity() const = 0;
 };
 
 #endif
